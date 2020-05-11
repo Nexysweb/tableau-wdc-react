0842e4262f6865d65f33a254c89bbdcc463c5168
John B <john@nexys.ch>
2020-05-11 14:34:04 +0200

    puyblish pages

---
+++ docs/precache-manifest.5a28ac7b24d953dd348aa4c4b687a217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e5b89fa4b73020d438e1286128a624",
+    "url": "/tableau-wdc-react/index.html"
+  },
+  {
+    "revision": "bf00313fcdbd0085192d",
+    "url": "/tableau-wdc-react/static/js/2.3c763a37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tableau-wdc-react/static/js/2.3c763a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac5d695db28a36394f6",
+    "url": "/tableau-wdc-react/static/js/main.4ff21bca.chunk.js"
+  },
+  {
+    "revision": "bdcecca4ad8f4f67c365",
+    "url": "/tableau-wdc-react/static/js/runtime-main.5f6472e7.js"
+  }
+]);
