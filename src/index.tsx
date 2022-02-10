9467c6f03079ce4597731b44526ee8aa4478e6a4
John B <johan@nexys.ch>
2022-02-10 18:24:38 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./example";
-import * as Config from './config';
 
 ReactDOM.render(
   <React.StrictMode>
     <p>Make sure this page is run from either Tableau directly or the <a href="https://tableau.github.io/webdataconnector/Simulator/">simulator</a> but <b>not</b> directly in the browser</p>
     <App />
-    <p><a href={Config.github.version}>{Config.version}</a></p>
   </React.StrictMode>,
   document.getElementById("root")
 );
