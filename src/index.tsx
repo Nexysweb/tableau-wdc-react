1ca4443144ea1a23744857cf4564187375ac0b7a
John B <johan@nexys.ch>
2022-02-10 18:13:36 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./example";
-//import App from './lib/tssaconnector'
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <p>Make sure this page is run from either Tableau or the simmulator</p>
+    <p>Make sure this page is run from either Tableau directly or the <a href="https://tableau.github.io/webdataconnector/Simulator/">simulator</a> but <b>not</b> directly in the browser</p>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
